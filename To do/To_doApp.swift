a4839d1abdf064e0d3d274fadfb3427ff695a747
TraianC299 <traianciochina@gmail.com>
2022-08-08 11:37:52 +0300

    Initial Commit

---
+++ To do/To_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doApp.swift
+//  To do
+//
+//  Created by Traian on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct To_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
